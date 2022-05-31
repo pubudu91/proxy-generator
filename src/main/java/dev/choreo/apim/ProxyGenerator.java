76e1657812098ec02a11651b1e1909fec936756f
Pubudu Fernando <pubuduf@wso2.com>
2022-05-31 12:12:16 +0530

    Refactor the generator to work based on API artifacts

--- src/main/java/dev/choreo/apim/ProxyGenerator.java
+++ src/main/java/dev/choreo/apim/ProxyGenerator.java
@@ -36,18 +36,28 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class ProxyGenerator {
 
-    private static final Path WORKING_DIR = Paths.get("/Users/pubuduf/poc/mediation-gen");
+    private static final Path WORKING_DIR = Path.of(System.getProperty("user.dir"));
 
     public static void main(String[] args) throws IOException {
         setBallerinaHomeDistribution();
-        Project project = loadProject("pizza_shack");
+        Path projectPath = generateProject();
+        Path openAPIDef = getOpenAPIDefinition(args[0], projectPath);
+        generateService(openAPIDef, projectPath);
+
+        Project project = loadProject(projectPath);
         Module module = project.currentPackage().getDefaultModule();
         Document serviceDoc = getOpenAPIGeneratedService(module);
         TextDocument txtDoc = serviceDoc.textDocument();
@@ -61,7 +71,63 @@ public class ProxyGenerator {
         txtDoc = txtDoc.apply(docChange);
         updatedServiceDoc = updatedServiceDoc.modify().withContent(txtDoc.toString()).apply();
 
-        writeToFile(updatedServiceDoc);
+        writeToFile(updatedServiceDoc, projectPath);
+    }
+
+    private static void generateService(Path openAPIDef, Path projectPath) {
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.command(System.getenv("SHELL"), "-c",
+                        String.format("bal openapi -i %s -o %s --mode service", openAPIDef.toString(),
+                                      projectPath.toString()));
+
+        try {
+            Process process = builder.start();
+            int exitVal = process.waitFor();
+
+            if (exitVal != 0) {
+                throw new RuntimeException("'bal openapi' command exited unexpectedly");
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Path getOpenAPIDefinition(String file, Path projectPath) {
+        try (ZipFile zipFile = new ZipFile(file)) {
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+
+
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+
+                if (!entry.getName().endsWith("/Definitions/swagger.yaml")) {
+                    continue;
+                }
+
+                Path openapiFile = Files.createFile(projectPath.resolve("proxy.yaml"));
+
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(zipFile.getInputStream(entry)));
+                     FileWriter writer = new FileWriter(openapiFile.toString())) {
+                    String content = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+                    writer.write(content);
+                    writer.flush();
+                    return openapiFile;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            throw new AssertionError("Failed to read OpenAPI definition from: " + file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Path generateProject() throws IOException {
+        String name = String.format("bal_proxy_%s", Instant.now().toEpochMilli());
+        Path projectPath = Files.createDirectory(WORKING_DIR.resolve(name));
+        Files.createFile(WORKING_DIR.resolve(name).resolve("Ballerina.toml"));
+        return projectPath;
     }
 
     private static void setBallerinaHomeDistribution() {
@@ -84,16 +150,15 @@ public class ProxyGenerator {
         }
     }
 
-    private static void writeToFile(Document doc) throws IOException {
+    private static void writeToFile(Document doc, Path projectPath) throws IOException {
         PrintWriter writer = new PrintWriter(new FileWriter(
-                Paths.get(WORKING_DIR.toString(), "pizza_shack", "_generated_" + doc.name()).toString()));
+                Paths.get(projectPath.toString(), "_generated_" + doc.name()).toString()));
         writer.write(doc.textDocument().toString());
         writer.flush();
         writer.close();
     }
 
-    private static Project loadProject(String sourceFilePath) {
-        Path projectPath = WORKING_DIR.resolve(sourceFilePath);
+    private static Project loadProject(Path projectPath) {
         BuildOptions defaultOptions = BuildOptions.builder().setOffline(true).setDumpBirFile(true).build();
         return ProjectLoader.loadProject(projectPath, defaultOptions);
     }
@@ -103,7 +168,7 @@ public class ProxyGenerator {
 
         for (DocumentId documentId : documentIds) {
             Document doc = module.document(documentId);
-            if (doc.name().contains("service")) {
+            if (doc.name().equals("proxy_service.bal")) {
                 return doc;
             }
         }
