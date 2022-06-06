4549f4c096c4ecb737e918c73be45801b7028e11
Pubudu Fernando <pubuduf@wso2.com>
2022-06-07 02:05:09 +0530

    Move mediation sequence gen logic to the code gen

--- src/main/java/dev/choreo/apim/CodeGenerator.java
+++ src/main/java/dev/choreo/apim/CodeGenerator.java
@@ -19,6 +19,12 @@
 package dev.choreo.apim;
 
 import com.google.gson.JsonObject;
+import dev.choreo.apim.artifact.model.Operation;
+import dev.choreo.apim.artifact.model.Policy;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static dev.choreo.apim.utils.Names.BACKEND_RESPONSE;
 import static dev.choreo.apim.utils.Names.ERROR;
@@ -31,39 +37,111 @@ public class CodeGenerator {
     private final String inflowTemplate;
     private final String outflowTemplate;
     private final String faultflowTemplate;
+    private final Map<String, Operation> operations;
+    private final PolicyManager policyManager;
+    private final Set<String> imports;
 
-    public CodeGenerator(String inflowTemplate, String outflowTemplate, String faultflowTemplate) {
+
+    public CodeGenerator(String inflowTemplate, String outflowTemplate, String faultflowTemplate,
+                         PolicyManager policyManager, Map<String, Operation> operations) {
         this.inflowTemplate = inflowTemplate;
         this.outflowTemplate = outflowTemplate;
         this.faultflowTemplate = faultflowTemplate;
+        this.operations = operations;
+        this.policyManager = policyManager;
+        this.imports = new HashSet<>();
     }
 
-    public String generateInFlowPolicyInvocation(PolicyPackage pkg) {
+    public String generateInflow(CodeContext ctx) {
+        Operation operation = this.operations.get(ctx.resourceMethodSignature());
+
+        if (operation == null || operation.getOperationPolicies().getRequest().isEmpty()) {
+            return null;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (Policy policy : operation.getOperationPolicies().getRequest()) {
+            PolicyPackage pkg = policyManager.get(policy.getPolicyName(), policy.getPolicyVersion());
+            builder.append(generateInFlowPolicyInvocation(pkg)).append('\n');
+            String pkgName = String.format("%s/%s", pkg.org(), pkg.name());
+            this.imports.add(pkgName);
+        }
+
+        return builder.toString();
+    }
+
+    public String generateOutflow(CodeContext ctx) {
+        Operation operation = this.operations.get(ctx.resourceMethodSignature());
+
+        if (operation == null || operation.getOperationPolicies().getResponse().isEmpty()) {
+            return null;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (Policy policy : operation.getOperationPolicies().getResponse()) {
+            PolicyPackage pkg = policyManager.get(policy.getPolicyName(), policy.getPolicyVersion());
+            builder.append(generateOutFlowPolicyInvocation(pkg)).append('\n');
+            String pkgName = String.format("%s/%s", pkg.org(), pkg.name());
+            this.imports.add(pkgName);
+        }
+
+        return builder.toString();
+    }
+
+    public String generateFaultFlow(CodeContext ctx) {
+        Operation operation = this.operations.get(ctx.resourceMethodSignature());
+
+        if (operation == null || operation.getOperationPolicies().getFault().isEmpty()) {
+            return null;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (Policy policy : operation.getOperationPolicies().getFault()) {
+            PolicyPackage pkg = policyManager.get(policy.getPolicyName(), policy.getPolicyVersion());
+            builder.append(generateFaultFlowPolicyInvocation(pkg)).append('\n');
+            String pkgName = String.format("%s/%s", pkg.org(), pkg.name());
+            this.imports.add(pkgName);
+        }
+
+        return builder.toString();
+    }
+
+    public String generateImports() {
+        StringBuilder builder = new StringBuilder();
+
+        for (String imprt : this.imports) {
+            builder.append(String.format("import %s;\n", imprt));
+        }
+
+        return builder.toString();
+    }
+
+    private String generateInFlowPolicyInvocation(PolicyPackage pkg) {
         if (pkg.getInFlowPolicy().isEmpty()) {
             return "";
         }
-
         JsonObject func = pkg.getInFlowPolicy().get();
         String fnCall = format("%s:%s(%s)", pkg.name(), func.get("name").getAsString(), INCOMING_REQUEST);
         return format(this.inflowTemplate, fnCall);
     }
 
-    public String generateOutFlowPolicyInvocation(PolicyPackage pkg) {
+    private String generateOutFlowPolicyInvocation(PolicyPackage pkg) {
         if (pkg.getOutFlowPolicy().isEmpty()) {
             return "";
         }
-
         JsonObject func = pkg.getOutFlowPolicy().get();
         String fnCall = format("%s:%s(%s, %s)", pkg.name(), func.get("name").getAsString(), BACKEND_RESPONSE,
                                INCOMING_REQUEST);
         return format(this.outflowTemplate, fnCall);
     }
 
-    public String generateFaultFlowPolicyInvocation(PolicyPackage pkg) {
+    private String generateFaultFlowPolicyInvocation(PolicyPackage pkg) {
         if (pkg.getFaultFlowPolicy().isEmpty()) {
             return "";
         }
-
         JsonObject func = pkg.getFaultFlowPolicy().get();
         String fnCall = format("%s:%s(%s, %s, %s, %s)", pkg.name(), func.get("name").getAsString(), ERROR_FLOW_RESPONSE,
                                ERROR, BACKEND_RESPONSE, INCOMING_REQUEST);
--- src/main/java/dev/choreo/apim/SyntaxTreeTransformer.java
+++ src/main/java/dev/choreo/apim/SyntaxTreeTransformer.java
@@ -19,7 +19,6 @@
 package dev.choreo.apim;
 
 import dev.choreo.apim.artifact.model.Operation;
-import dev.choreo.apim.artifact.model.Policy;
 import dev.choreo.apim.code.builders.DoBlock;
 import io.ballerina.compiler.syntax.tree.FunctionBodyBlockNode;
 import io.ballerina.compiler.syntax.tree.FunctionDefinitionNode;
@@ -27,12 +26,10 @@ import io.ballerina.compiler.syntax.tree.FunctionSignatureNode;
 import io.ballerina.compiler.syntax.tree.ModuleMemberDeclarationNode;
 import io.ballerina.compiler.syntax.tree.ModulePartNode;
 import io.ballerina.compiler.syntax.tree.Node;
-import io.ballerina.compiler.syntax.tree.NodeList;
 import io.ballerina.compiler.syntax.tree.NodeVisitor;
 import io.ballerina.compiler.syntax.tree.ReturnTypeDescriptorNode;
 import io.ballerina.compiler.syntax.tree.ServiceDeclarationNode;
 import io.ballerina.compiler.syntax.tree.SyntaxKind;
-import io.ballerina.compiler.syntax.tree.Token;
 import io.ballerina.projects.Document;
 import io.ballerina.tools.text.LineRange;
 import io.ballerina.tools.text.TextDocumentChange;
@@ -40,10 +37,8 @@ import io.ballerina.tools.text.TextEdit;
 import io.ballerina.tools.text.TextRange;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import static dev.choreo.apim.utils.Names.BACKEND_ENDPOINT;
 import static dev.choreo.apim.utils.Names.BACKEND_RESPONSE;
@@ -51,7 +46,6 @@ import static dev.choreo.apim.utils.Names.CALLER;
 import static dev.choreo.apim.utils.Names.ERROR_FLOW_RESPONSE;
 import static dev.choreo.apim.utils.Names.INCOMING_REQUEST;
 import static dev.choreo.apim.utils.Names.UPDATED_HEADERS;
-import static dev.choreo.apim.utils.Utils.buildOpKey;
 import static java.lang.String.format;
 
 public class SyntaxTreeTransformer extends NodeVisitor {
@@ -63,10 +57,7 @@ public class SyntaxTreeTransformer extends NodeVisitor {
     private final PolicyManager policyManager;
     private CodeGenerator codegen;
     private List<TextEdit> edits;
-    private Map<String, Operation> operations;
-    private String functionName;
-    private String currentOperation;
-    private Set<String> imports;
+    private CodeContext ctx;
 
     public SyntaxTreeTransformer(String inflowTemplate, String outflowTemplate, String faultflowTemplate,
                                  PolicyManager policyManager) {
@@ -78,18 +69,24 @@ public class SyntaxTreeTransformer extends NodeVisitor {
 
     public TextDocumentChange modifyDoc(Document document, Map<String, Operation> operations) {
         this.edits = new ArrayList<>();
-        this.imports = new HashSet<>();
-        this.operations = operations;
-        this.codegen = new CodeGenerator(inflowTemplate, outflowTemplate, faultflowTemplate);
-        document.syntaxTree().rootNode().accept(this);
+        this.codegen = new CodeGenerator(inflowTemplate, outflowTemplate, faultflowTemplate, policyManager, operations);
+        visitNode(document.syntaxTree().rootNode());
+        this.edits.add(0, TextEdit.from(START_POS, codegen.generateImports()));
         return TextDocumentChange.from(this.edits.toArray(new TextEdit[0]));
+    }
+
+    private void visitNode(Node node) {
+        CodeContext prevCtx = this.ctx;
+        this.ctx = new CodeContext(prevCtx, node);
+        node.accept(this);
+        this.ctx = prevCtx;
     }
 
     @Override
     public void visit(ModulePartNode modulePartNode) {
         for (ModuleMemberDeclarationNode member : modulePartNode.members()) {
             if (member.kind() == SyntaxKind.SERVICE_DECLARATION) {
-                member.accept(this);
+                visitNode(member);
             }
         }
     }
@@ -98,20 +95,15 @@ public class SyntaxTreeTransformer extends NodeVisitor {
     public void visit(ServiceDeclarationNode serviceDeclarationNode) {
         for (Node member : serviceDeclarationNode.members()) {
             if (member.kind() == SyntaxKind.RESOURCE_ACCESSOR_DEFINITION) {
-                member.accept(this);
+                visitNode(member);
             }
         }
     }
 
     @Override
     public void visit(FunctionDefinitionNode functionDefinitionNode) {
-        this.functionName = functionDefinitionNode.functionName().text();
-        this.currentOperation = buildOpKey(this.functionName,
-                                           getResourcePath(functionDefinitionNode.relativeResourcePath()));
-        functionDefinitionNode.functionSignature().accept(this);
-        functionDefinitionNode.functionBody().accept(this);
-        this.functionName = null;
-        this.currentOperation = null;
+        visitNode(functionDefinitionNode.functionSignature());
+        visitNode(functionDefinitionNode.functionBody());
     }
 
     @Override
@@ -140,83 +132,16 @@ public class SyntaxTreeTransformer extends NodeVisitor {
         DoBlock doBlock = new DoBlock(closingBraceLR.startLine().offset() / 4 + 1);
         // TODO: 2022-06-03 Move to code gen
         String code = doBlock
-                .addStatement(generateInflow())
-                .addStatement(getBackendHTTPCall(this.functionName))
-                .addStatement(generateOutflow())
+                .addStatement(this.codegen.generateInflow(this.ctx))
+                .addStatement(getBackendHTTPCall(this.ctx.resourceMethodName()))
+                .addStatement(this.codegen.generateOutflow(this.ctx))
                 .addStatement(format("check %s->respond(%s);", CALLER, BACKEND_RESPONSE))
                 .addStatementToOnFail(format("http:Response %s = createDefaultErrorResponse();", ERROR_FLOW_RESPONSE))
-                .addStatementToOnFail(generateFaultFlow())
+                .addStatementToOnFail(this.codegen.generateFaultFlow(this.ctx))
                 .addStatementToOnFail(format("check %s->respond(%s);", CALLER, ERROR_FLOW_RESPONSE))
                 .build();
         TextEdit body = TextEdit.from(start, code);
         edits.add(body);
-    }
-
-    // TODO: 2022-06-03 Move these code generation parts to the code generator
-    private String generateInflow() {
-        StringBuilder builder = new StringBuilder();
-
-        Operation operation = this.operations.get(this.currentOperation);
-
-        if (operation == null || operation.getOperationPolicies().getRequest().isEmpty()) {
-            return null;
-        }
-
-        for (Policy policy : operation.getOperationPolicies().getRequest()) {
-            PolicyPackage pkg = policyManager.get(policy.getPolicyName(), policy.getPolicyVersion());
-            builder.append(codegen.generateInFlowPolicyInvocation(pkg)).append('\n');
-            String pkgName = String.format("%s/%s", pkg.org(), pkg.name());
-            if (!this.imports.contains(pkgName)) {
-                this.edits.add(0, TextEdit.from(START_POS, String.format("import %s;\n", pkgName)));
-                this.imports.add(pkgName);
-            }
-        }
-
-        return builder.toString();
-    }
-
-    private String generateOutflow() {
-        StringBuilder builder = new StringBuilder();
-
-        Operation operation = this.operations.get(this.currentOperation);
-
-        if (operation == null || operation.getOperationPolicies().getResponse().isEmpty()) {
-            return null;
-        }
-
-        for (Policy policy : operation.getOperationPolicies().getResponse()) {
-            PolicyPackage pkg = policyManager.get(policy.getPolicyName(), policy.getPolicyVersion());
-            builder.append(codegen.generateOutFlowPolicyInvocation(pkg)).append('\n');
-            String pkgName = String.format("%s/%s", pkg.org(), pkg.name());
-            if (!this.imports.contains(pkgName)) {
-                this.edits.add(0, TextEdit.from(START_POS, String.format("import %s;\n", pkgName)));
-                this.imports.add(pkgName);
-            }
-        }
-
-        return builder.toString();
-    }
-
-    private String generateFaultFlow() {
-        StringBuilder builder = new StringBuilder();
-
-        Operation operation = this.operations.get(this.currentOperation);
-
-        if (operation == null || operation.getOperationPolicies().getFault().isEmpty()) {
-            return null;
-        }
-
-        for (Policy policy : operation.getOperationPolicies().getFault()) {
-            PolicyPackage pkg = policyManager.get(policy.getPolicyName(), policy.getPolicyVersion());
-            builder.append(codegen.generateFaultFlowPolicyInvocation(pkg)).append('\n');
-            String pkgName = String.format("%s/%s", pkg.org(), pkg.name());
-            if (!this.imports.contains(pkgName)) {
-                this.edits.add(0, TextEdit.from(START_POS, String.format("import %s;\n", pkgName)));
-                this.imports.add(pkgName);
-            }
-        }
-
-        return builder.toString();
     }
 
     private String getBackendHTTPCall(String functionName) {
@@ -227,40 +152,11 @@ public class SyntaxTreeTransformer extends NodeVisitor {
             builder.append(format("map<string|string[]> %s = copyRequestHeaders(%s);\n", UPDATED_HEADERS,
                                   INCOMING_REQUEST));
             builder.append(format("http:Response %s = check %s->%s(\"...\", %s);\n", BACKEND_RESPONSE, BACKEND_ENDPOINT,
-                                  this.functionName, UPDATED_HEADERS));
+                                  this.ctx.resourceMethodName(), UPDATED_HEADERS));
             return builder.toString();
         }
 
         return format("http:Response %s = check %s->%s(\"...\", %s);\n", BACKEND_RESPONSE, BACKEND_ENDPOINT,
-                      this.functionName, INCOMING_REQUEST);
-    }
-
-    private String getResourcePath(NodeList<Node> pathSegments) {
-        StringBuilder pathBuilder = new StringBuilder("/");
-
-        for (Node pathSegment : pathSegments) {
-            switch (pathSegment.kind()) {
-                case IDENTIFIER_TOKEN:
-                    String path = ((Token) pathSegment).text();
-                    if (path.startsWith("'")) {
-                        path = path.substring(1);
-                    }
-                    pathBuilder.append(path);
-                    break;
-                case SLASH_TOKEN:
-                    pathBuilder.append("/");
-                    break;
-                case RESOURCE_PATH_SEGMENT_PARAM:
-                    pathBuilder.append('*');
-                    break;
-                case RESOURCE_PATH_REST_PARAM:
-                    pathBuilder.append("**");
-                    break;
-                default:
-                    throw new AssertionError("Unexpected syntax kind: " + pathSegment.kind());
-            }
-        }
-
-        return pathBuilder.toString();
+                      this.ctx.resourceMethodName(), INCOMING_REQUEST);
     }
 }
